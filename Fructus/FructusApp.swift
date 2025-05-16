cdfedaeb140085e44f5653dbcba015956862c2fb
Edu Caubilla <educaubilla@gmail.com>
2025-05-16 13:28:03 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Edu Caubilla on 16/5/25.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
